4f6e97dab9a40c8ee01aaae4bb82d230f1fbcacc
coleleg <coleleg@gmail.com>
2022-04-25 11:37:58 -0400

    add more skeleton to react pages/components

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Navbar = () => {
+    return (
+        <Nav>
+            
+        </Nav>
+    );
+};
+
+const Nav = styled.div`
+`;
+
+export default Navbar;
---
+++ client/src/pages/Home.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <Header>
+            <SearchBar>
+
+            </SearchBar>
+            <Results>
+
+            </Results>
+        </Header>
+    );
+};
+
+const Header = styled.div`
+`
+const SearchBar = styled.form`
+`
+const Results = styled.div`
+`
+
+
+export default Home;
