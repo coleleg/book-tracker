9fe79c3c8a1577d8323f1e6062aff240ce009fc6
coleleg <coleleg@gmail.com>
2022-04-26 16:54:15 -0400

    add client side auth

---
+++ client/src/utils/auth.js
@@ -0,0 +1,39 @@
+import decode from 'jwt-decode';
+
+class AuthService {
+    getToken() {
+        return localStorage.getItem('user_token');
+    }
+
+    login(userToken) {
+        localStorage.setItem('user_token', userToken);
+        window.location.assign('/');
+    }
+
+    logout() {
+        localStorage.removeItem('user_token');
+        window.location.assign('/');
+    }
+
+    getUserProfile() {
+        return decode(this.getToken());
+    }
+
+    loggedIn() {
+        const token = this.getToken();
+        return !!token && !this.isTokenExpired(token);
+    }
+
+    expiredToken(token) {
+        try {
+            const decoded = decode(token);
+            if (decoded.exp < Date.now() / 1000) {
+                return true;
+            } return false;
+        } catch (err) {
+            return false;
+        }
+    }
+}
+
+export default new AuthService();
