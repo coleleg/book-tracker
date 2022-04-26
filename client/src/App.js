9e3f4d239f9072f8e64f33e36dc8ea6323210d76
coleleg <coleleg@gmail.com>
2022-04-26 17:17:06 -0400

    setup app js and add navbar skeleton

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Saved from './pages/Saved';
+import Current from './pages/Current';
+import Finished from './pages/Finished';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <>
+        <Navbar />
+        <Routes>
+          <Route exact path = '/' component={Home} />
+          <Route exact path = '/saved' component={Saved} />
+          <Route exact path = '/current' component={Current} />
+          <Route exact path = '/finished' component={Finished} />
+        </Routes>
+      </>
+    </Router>
   );
 }
 
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -4,12 +4,43 @@ import styled from 'styled-components';
 const Navbar = () => {
     return (
         <Nav>
-            
+            <Logo />
+            <Menu>
+                <a>
+                    <span>LOGIN</span>
+                </a>
+                <a>
+                    <span>SIGNUP</span>
+                </a>
+                <a>
+                    <span>LOGOUT</span>
+                </a>
+            </Menu>
         </Nav>
     );
 };
 
 const Nav = styled.div`
+    width: 100%;
+    height: 70px;
+    background-color: #5ADE86;
+    display: flex;
+    justify-content: space-between;
 `;
+
+const Logo = styled.div`
+`
+
+const Menu = styled.div`
+    padding: 5px;
+    display: flex;
+    align-items: center;
+
+    a {
+        margin-right: 2vw;
+        cursor: pointer;
+        font-weight: bold;
+    }
+`
 
 export default Navbar;
