ab351daa9ae0217d2209b7710a18bdf73e585561
coleleg <coleleg@gmail.com>
2022-05-19 16:26:41 -0400

    heroku

--- server/server.js
+++ server/server.js
@@ -9,13 +9,7 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')));
-}
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
