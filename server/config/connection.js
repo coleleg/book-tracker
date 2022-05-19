505a0a44531a52fcd970044001357a3894708dc6
coleleg <coleleg@gmail.com>
2022-05-19 16:44:50 -0400

    heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-mongoose.connect(process.env.MONGODB_URI || `mongodb+srv://` + process.env.DB_USER + `:` + process.env.DB_PW + `@cluster0.sxprl.mongodb.net/book-tracker?retryWrites=true&w=majority`,
+mongoose.connect(process.env.MONGODB_URI ||`mongodb://localhost:27017/book-search-engine`, 
     {
         useNewUrlParser: true,
         useUnifiedTopology: true
