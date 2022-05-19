f57d1f6488e6e05c9922841d99278daa66172199
coleleg <coleleg@gmail.com>
2022-05-19 19:44:39 -0400

    heroku

--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
-const uri = process.env.MONGODB_URI;
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -15,7 +14,7 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+    res.sendFile(path.join(__dirname, '../client/build'));
 });
 
 app.use(routes);
