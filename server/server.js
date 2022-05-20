e2462232d01bb7554ba8239821e09895fc931e34
coleleg <coleleg@gmail.com>
2022-05-19 20:16:04 -0400

    comment out app.use(routes) in server.js

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
-app.use(routes);
+// app.use(routes);
 
 db.once('open', () => {
         app.listen(PORT, () => {
